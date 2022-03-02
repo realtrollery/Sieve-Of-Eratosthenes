bf24adfbb77bfe7b87fe051a9cb958e955ae52c6
realtrollery <93254979+realtrollery@users.noreply.github.com>
2022-03-02 21:19:31 +0900

    Add files via upload

---
+++ Sieve.py
@@ -0,0 +1,11 @@
+def prime_list(n):
+    sieve = [True] * n
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i]:
+            for j in range(i + i, n, i):
+                sieve[j] = False
+    return [i for i in range(2, n) if sieve[i]]
+
+
+print(prime_list(100))
